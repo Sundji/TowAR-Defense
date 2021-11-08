04054d67beea8c6993f648bac67145289c75e4f0
Dora Facković <dora.fackovic@fer.hr>
2021-11-08 14:02:58 +0100

    Added FindClosestTarget to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 public class GameManager : SingletonBehaviour<GameManager>
 {
     // DOHVAT VARIJABLI ILI FUNKCIJA: GameManager.Instance.ime-varijable-ili-funkcije
     // ZA KORISTENJE AWAKE FUNKCIJE TREBA NAPRAVITI NJEN OVERRIDE I KAO PRVI KORAK OSTAVITI base.Awake() (koristi se u SingletonBehaviour)
+
+    public List<Pawn> _friendlyPawns;
+    public List<Pawn> _enemyPawns;
+    private int _towersLeft = 0;
 
     private void Start()
     {
@@ -10,6 +17,62 @@ public class GameManager : SingletonBehaviour<GameManager>
 
     private void OnPawnCreated(Pawn pawn)
     {
+        if ((pawn._type == Pawn.PawnType.FRIENDLY) || (pawn._type == Pawn.PawnType.TOWER))
+        {
+            _friendlyPawns.Add(pawn);
 
+            if (pawn._type == Pawn.PawnType.TOWER)
+            {
+                _towersLeft += 1;
+            }
+
+        }
+        else
+        {
+            _enemyPawns.Add(pawn);
+        }
+
+    }
+
+    public Pawn FindClosestTarget(Vector3 position, Pawn.PawnType attackerType)
+    {
+        float minDistance = Mathf.Infinity;
+        Pawn closest = null;
+        if ((attackerType == Pawn.PawnType.FRIENDLY) || (attackerType == Pawn.PawnType.TOWER))
+        {
+            foreach (Pawn p in _enemyPawns)
+            {
+                float trenDistance = Vector3.Distance(position, p._transform.position);
+                if (trenDistance < minDistance)
+                {
+                    minDistance = trenDistance;
+                    closest = p;
+                }
+            }
+
+        } else if (attackerType == Pawn.PawnType.ENEMY_MELEE)
+        {
+            foreach (Pawn p in _friendlyPawns)
+            {
+                float trenDistance = Vector3.Distance(position, p._transform.position);
+                if ((p._type == Pawn.PawnType.FRIENDLY) && (trenDistance < minDistance))
+                {
+                    minDistance = trenDistance;
+                    closest = p;
+                }
+            }
+        } else
+        {
+            foreach (Pawn p in _friendlyPawns)
+            {
+                float trenDistance = Vector3.Distance(position, p._transform.position);
+                if ((p._type == Pawn.PawnType.TOWER) && (trenDistance < minDistance))
+                {
+                    minDistance = trenDistance;
+                    closest = p;
+                }
+            }
+        }
+        return closest;
     }
 }
--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class Pawn : MonoBehaviour
 {
+    public enum PawnType { FRIENDLY, TOWER, ENEMY_MELEE, ENEMY_RANGE }
+    public PawnType _type;
+    public Transform _transform;
+
+
+    private void Update()
+    {
+        if (this._type ==PawnType.FRIENDLY)
+        {
+            Debug.Log(GameManager.Instance.FindClosestTarget(_transform.position, _type)._type);
+        }
+    }
+
 
 }
