f7577641a8005b76010c6b43d6cc99bad0b5df51
Dora Facković <dora.fackovic@fer.hr>
2021-11-13 19:53:35 +0100

    added Bullet class, bullet movement in direction, onTriggerEnter decreases health

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector3 _direction;
+    bool _isFired;
+    float _flySpeed = 5f;
+    float _damage = 25f;
+
+    public void Prepare(Vector3 direction)
+    {
+        _direction = direction;
+        _isFired = true;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isFired)
+        {
+            Debug.Log(_direction);
+            transform.position += _direction * _flySpeed * Time.deltaTime;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Pawn>() != null)
+        {
+            other.GetComponent<Pawn>().loseHealth(_damage);
+            Destroy(gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/Pawn.cs
+++ Assets/Scripts/Pawn.cs
@@ -2,18 +2,41 @@ using UnityEngine;
 
 public class Pawn : MonoBehaviour
 {
+    private float MAX_HEALTH = 100f;
     public enum PawnType { FRIENDLY, TOWER, ENEMY_MELEE, ENEMY_RANGE }
     public PawnType _type;
     public Transform _transform;
+    public Bullet _bullet;
+    public float _health;
 
+    bool once = true;
+    
+
+    private void Start()
+    {
+        _transform = this.transform;
+        _health = MAX_HEALTH;
+    }
 
     private void Update()
     {
-        if (this._type ==PawnType.FRIENDLY)
+        if (this._type == PawnType.FRIENDLY && once)
         {
-            Debug.Log(GameManager.Instance.FindClosestTarget(_transform.position, _type)._type);
+            //Debug.Log(GameManager.Instance.FindClosestTarget(_transform.position, _type)._type);
+            Pawn nextTarget = GameManager.Instance.FindClosestTarget(_transform.position, _type);
+
+            Bullet firedBullet = Instantiate(_bullet, _transform.Find("SpawnProjectile").transform);
+
+            Vector3 fireDirection = (nextTarget.transform.position - _transform.position).normalized;
+
+            firedBullet.GetComponent<Bullet>().Prepare(fireDirection);
+            once = false;
+
         }
     }
 
-
+    public void loseHealth(float recievedDamage)
+    {
+        _health -= recievedDamage;
+    }
 }
