945f5017bc38095b8c5cbea367fe756b0a082e10
Ana Sundji <asundji@gmail.com>
2022-01-01 14:37:52 +0100

    Unused classes removed.

--- Assets/Scripts/CustomEvent.cs
+++ Assets/Scripts/CustomEvent.cs
@@ -4,13 +4,3 @@ public class CustomEvent<T0> : UnityEvent<T0>
 {
 
 }
-
-public class CustomEvent<T0, T1> : UnityEvent<T0, T1>
-{
-
-}
-
-public class CustomEvent<T0, T1, T2> : UnityEvent<T0, T1, T2>
-{
-
-}
